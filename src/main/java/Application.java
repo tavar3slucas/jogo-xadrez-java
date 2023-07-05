8f18ebe7b284053ed71b9630da2b8fdbea3cceb0
Lucas Tavares <t.luuccas@gmail.com>
2023-07-05 12:15:13 -0300

    Criando primeia classe, position

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,8 @@
+import boardgame.Position;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("testando");
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
