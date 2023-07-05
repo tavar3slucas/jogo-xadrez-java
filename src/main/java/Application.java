23970ea4558a599ce2c31895b9de7f92c0d5e8df
Lucas Tavares <t.luuccas@gmail.com>
2023-07-05 12:25:29 -0300

    Iniciando implementaçaõ das classes board e piece

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,8 @@
-import boardgame.Position;
+import boardgame.Board;
 
 public class Application {
     public static void main(String[] args) {
-        Position pos = new Position(3,5);
-        System.out.println(pos);
+        Board board = new Board(8,5);
+
     }
 }
